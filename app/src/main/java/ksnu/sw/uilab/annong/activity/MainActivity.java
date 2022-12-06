febc1a99e73c362bf2ea631d67ac562a2411d8ea
song-wooseok <sws981202@gmail.com>
2022-12-06 19:31:30 +0900

    init AnNong Project

---
+++ app/src/main/java/ksnu/sw/uilab/annong/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package ksnu.sw.uilab.annong.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import ksnu.sw.uilab.annong.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
