221f4266b816552943c3fc5ce41bbc39731cc63e
song-wooseok <sws981202@gmail.com>
2023-01-03 18:19:20 +0900

    feat: 각 데이터 타입에 맞는 람다식 검증 로직 enum에 추가

--- app/src/main/java/ksnu/sw/uilab/annong/utils/enums/CustomDataType.java
+++ app/src/main/java/ksnu/sw/uilab/annong/utils/enums/CustomDataType.java
@@ -6,16 +6,22 @@ import java.util.Arrays;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import ksnu.sw.uilab.annong.utils.enums.validator.DataTypeValidator;
+import ksnu.sw.uilab.annong.utils.enums.validator.exception.NotCorrectDataTypeException;
 
 @FunctionalInterface
 interface validateFunction<T>{
-    void apply(T t) throws Exception;
+    void apply(T t) throws NotCorrectDataTypeException;
 }
 
+@RequiresApi(api = VERSION_CODES.N)
 public enum CustomDataType {
     NUMBER(0, "숫자", (data)->{
-
+        DataTypeValidator.validateNumberTypeData(data);
     }),
+    /*
+    * 추후 텍스트 타입의 데이터에 대해서 검증 조건이 추가될 경우 람다식으로 메서드 작성
+    * */
     STRING(1, "텍스트", (data)->{});
 
     private final int index;
@@ -43,7 +49,7 @@ public enum CustomDataType {
     }
 
     @RequiresApi(api = VERSION_CODES.N)
-    public void validate(String data) throws Exception{
+    public void validate(String data) throws NotCorrectDataTypeException{
         validateExpression.apply(data);
     }
 }
